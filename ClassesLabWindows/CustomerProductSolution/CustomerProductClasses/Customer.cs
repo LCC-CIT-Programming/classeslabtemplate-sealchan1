584e1d92b041c6a774c32a205ff05dfa98583630
sealchan1 <sealchan@yahoo.com>
2021-04-19 12:57:47 -0700

    Problem 1

--- ClassesLabWindows/CustomerProductSolution/CustomerProductClasses/Customer.cs
+++ ClassesLabWindows/CustomerProductSolution/CustomerProductClasses/Customer.cs
@@ -8,5 +8,93 @@ namespace CustomerProductClasses
 {
     public class Customer
     {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string phone;
+
+        /// <summary>
+        /// Id - Customer identification number
+        /// </summary>
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        /// <summary>
+        /// FirstName - Customer first name
+        /// </summary>
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        /// <summary>
+        /// LastName - Customer last name
+        /// </summary>
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        /// <summary>
+        /// Email - Customer email
+        /// </summary>
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        /// <summary>
+        /// Phone - Customer phone
+        /// </summary>
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Customer() 
+        {
+            firstName = "unknown";
+            lastName = "unknown";
+            email = "unknown";
+            phone = "unknown";
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="id">Customer id number</param>
+        /// <param name="first">Customer first name</param>
+        /// <param name="last">Customer last name</param>
+        /// <param name="email">Customer email</param>
+        /// <param name="phone">Customer phone number</param>
+        public Customer(int id, string first, string last, string email, string phone)
+        {
+            this.id = id;
+            firstName = first;
+            lastName = last;
+            this.email = email;
+            this.phone = phone;
+        }
+
+        /// <summary>
+        /// ToString() - Provides an override to the default ToString() method
+        /// </summary>
+        /// <returns>Customer first and last name combined</returns>
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2} {3} {4}", id.ToString(), firstName, lastName, email, phone);
+        }
     }
 }
+
--- ClassesLabWindows/CustomerProductSolution/CustomerProductTests/Program.cs
+++ ClassesLabWindows/CustomerProductSolution/CustomerProductTests/Program.cs
@@ -12,11 +12,17 @@ namespace CustomerProductTests
     {
         static void Main(string[] args)
         {
-
+            // Product
             TestProductConstructors();
             TestProductToString();
             TestProductPropertyGetters();
             TestProductPropertySetters();
+
+            // Customer
+            TestCustomerConstructors();
+            TestCustomerToString();
+            TestCustomerPropertyGetters();
+            TestCustomerPropertySetters();
 
             Console.ReadLine();
         }
@@ -66,6 +72,54 @@ namespace CustomerProductTests
             p1.UnitPrice = 200;
             p1.QuantityOnHand = 20;
             Console.WriteLine("Expecting 2, T000, 200, First product, 20 " + p1);
+            Console.WriteLine();
+        }
+
+        static void TestCustomerConstructors()
+        {
+            Customer p1 = new Customer();
+            Customer p2 = new Customer(1, "Abby", "Armstrong", "aarmstrong@strong.com", "555-555-5555");
+
+            Console.WriteLine("Testing both constructors");
+            Console.WriteLine("Default constructor.  Expecting default values. " + p1.ToString());
+            Console.WriteLine("Overloaded constructor.  Expecting 1 Abby Armstrong aarmstrong@strong.com 555-555-5555 : " + p2.ToString());
+            Console.WriteLine();
+        }
+
+        static void TestCustomerToString()
+        {
+            Customer p1 = new Customer(1, "Abby", "Armstrong", "aarmstrong@strong.com", "555-555-5555");
+
+            Console.WriteLine("Testing ToString");
+            Console.WriteLine("Expecting 1 Abby Armstrong aarmstrong@strong.com 555-555-5555 : " + p1.ToString());
+            Console.WriteLine("Expecting 1 Abby Armstrong aarmstrong@strong.com 555-555-5555 : " + p1);
+            Console.WriteLine();
+        }
+
+        static void TestCustomerPropertyGetters()
+        {
+            Customer p1 = new Customer(1, "Abby", "Armstrong", "aarmstrong@strong.com", "555-555-5555");
+
+            Console.WriteLine("Testing getters");
+            Console.WriteLine("Id.  Expecting 1. " + p1.Id);
+            Console.WriteLine("FirstName.  Expecting Abby. " + p1.FirstName);
+            Console.WriteLine("LastName.  Expecting Armstrong. " + p1.LastName);
+            Console.WriteLine("Email.  Expecting aarmstrong@strong.com. " + p1.Email);
+            Console.WriteLine("Phone.  Expecting 555-555-5555. " + p1.Phone);
+            Console.WriteLine();
+        }
+
+        static void TestCustomerPropertySetters()
+        {
+            Customer p1 = new Customer(1, "Abby", "Armstrong", "aarmstrong@strong.com", "555-555-5555");
+
+            Console.WriteLine("Testing setters");
+            p1.Id = 2;
+            p1.FirstName = "Bart";
+            p1.LastName = "Barnstorm";
+            p1.Email = "bbarnstorm@storm.com";
+            p1.Phone = "777-777-7777";
+            Console.WriteLine("Expecting 2, Bart, Barnstorm, bbarnstorm@storm.com, 777-777-7777 : " + p1);
             Console.WriteLine();
         }
     }
